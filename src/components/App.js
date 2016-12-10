89f909e3983777b3359887173378eec99db6db10
osnth01 <osnth01@gmail.com>
2016-12-10 15:48:15 -0500

    add router

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router'
 import AppStyle from './App.css'
 
-const App = () => <div className={AppStyle.app}><h1>Hello React!</h1></div>
+const App = (props) => (
+  <div className={AppStyle.app}>
+    <ul role="nav">
+      <li><Link to="/">Hello</Link></li>
+      <li><Link to="/goodbye">GoodBye</Link></li>
+    </ul>
+
+    {props.children}
+  </div>
+)
 
 export default App
